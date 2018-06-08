42cb9b73b6dbf162ba3b12c667e92b1cc6569d76
Narek Keryan <narek.keryan@gmail.com>
2018-06-08 10:31:28 +0400

    login and register page forms

--- pages/users/login.js
+++ pages/users/login.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Layout from '../../components/layout';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { username: '', password: '' };
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleUsernameChange(e) {
+        this.setState({ username: e.target.value });
+    }
+
+    handlePasswordChange(e) {
+        this.setState({ password: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <Layout>
+            <Layout title="Login Page">
                 <h1>Login Page</h1>
                 <p>Open <code>/pages/users/login.js</code> to edit this file.</p>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="username">Username</Label>
+                        <Input type="text" name="username" id="username" placeholder="Username" onChange={this.handleUsernameChange} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password</Label>
+                        <Input type="password" name="password" id="password" placeholder="Password" onChange={this.handlePasswordChange} />
+                    </FormGroup>
+                    <Button color="primary" type="submit">Login</Button>
+                </Form>
             </Layout>
         );
     }
--- pages/users/register.js
+++ pages/users/register.js
@@ -1,12 +1,56 @@
 import React, { Component } from 'react';
+import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import Layout from '../../components/layout';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { username: '', email: '', password: '' };
+
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleUsernameChange(e) {
+        this.setState({ username: e.target.value });
+    }
+
+    handleEmailChange(e) {
+        this.setState({ email: e.target.value });
+    }
+
+    handlePasswordChange(e) {
+        this.setState({ password: e.target.value });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
     render() {
         return (
-            <Layout>
+            <Layout title="Register Page">
                 <h1>Register Page</h1>
                 <p>Open <code>/pages/users/register.js</code> to edit this file.</p>
+                <Form onSubmit={this.handleSubmit}>
+                    <FormGroup>
+                        <Label for="username">Username</Label>
+                        <Input type="text" name="username" id="username" placeholder="Username" onChange={this.handleUsernameChange} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="email" name="email" id="email" placeholder="Email" onChange={this.handleEmailChange} />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password">Password</Label>
+                        <Input type="password" name="password" id="password" placeholder="Password" onChange={this.handlePasswordChange} />
+                    </FormGroup>
+                    <Button color="primary" type="submit">Register</Button>
+                </Form>
             </Layout>
         );
     }
